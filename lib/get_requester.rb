fbb83c8760fa97c952a8d4e793660def9a530f00
Samiylo Kryshu <skryshu@gmail.com>
2020-04-09 18:09:19 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -9,7 +9,7 @@ class GetRequester
   end
   
   def get_response_body
-    uri = URI.parse(URL)
+    uri = @string_uri.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
   end
